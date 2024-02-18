96a5be1401fc9d4425eba30f7c3660e1940957a4
Ayyubi3 <76443084+Ayyubi3@users.noreply.github.com>
2024-02-18 15:19:04 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ sudo apt -qq -y update
 sudo apt -qq -y upgrade
 
 # Install basic packages
-packages=("git" "build-essential" "ripgrep" "unzip" "fd-find" "lazygit")
+packages=("git" "build-essential" "ripgrep" "unzip" "fd-find" "lazygit" "curl" "tar")
 
 for element in "${packages[@]}"; do
 	out "Installing $element"
@@ -27,3 +27,11 @@ npm -v
 
 sudo npm install neovim -g
 sudo apt-get -q -y install neovim
+
+out "Installing lazygit"
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit /usr/local/bin
+rm lazygit.tar.gz
+rm lazygit
