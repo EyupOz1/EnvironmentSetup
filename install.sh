9b525acb5d004f560305e096f91cbad4d9aff000
root <root@Ayyubi>
2024-02-18 15:15:15 +0100

    ljk

--- install.sh
+++ install.sh
@@ -1,23 +1,29 @@
+#!/bin/bash
+
 function out() {
-  GREEN='\033[0;32m'
-  RESET='\033[0m'
-  echo -e "${GREEN}$1${RESET}"
+	GREEN='\033[0;32m'
+	RESET='\033[0m'
+	echo -e "${GREEN}$1${RESET}"
 }
 
-out "adding repo and updating system"
+out "Add nvim ppa and Update/Upgrade"
 sudo add-apt-repository -y ppa:neovim-ppa/unstable
-sudo apt -y update
-sudo apt -y upgrade
+sudo apt -qq -y update
+sudo apt -qq -y upgrade
 
-out "installing other apt packages"
-sudo apt -y install git build-essential ripgrep unzip fd
-out "installing nvim"
-sudo apt -y install neovim
+# Install basic packages
+packages=("git" "build-essential" "ripgrep" "unzip" "fd-find")
+
+for element in "${packages[@]}"; do
+	out "Installing $element"
+	sudo apt-get -q -y install "$element"
+done
 
 out "installing node"
-curl -sL https://deb.nodesource.com/setup_20.x -o /tmp/nodesource_setup.sh 
-sudo bash /tmp/nodesource_setup.sh && sudo apt install nodejs -y 
-node -v 
-npm -v 
+curl -sL https://deb.nodesource.com/setup_20.x -o /tmp/nodesource_setup.sh
+sudo bash /tmp/nodesource_setup.sh && sudo apt-get -q install nodejs -y
+node -v
+npm -v
 
 sudo npm install neovim -g
+sudo apt-get -q -y install neovim
