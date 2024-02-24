e57ae750995b97d8945173e886f7f7a67bcab526
Ayyubi3 <76443084+Ayyubi3@users.noreply.github.com>
2024-02-24 01:01:05 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,8 +6,7 @@ function out() {
 	echo -e "${GREEN}$1${RESET}"
 }
 
-out "Add nvim ppa and Update/Upgrade"
-sudo add-apt-repository -y ppa:neovim-ppa/unstable
+out "Update/Upgrade"
 sudo apt -qq -y update
 sudo apt -qq -y upgrade
 
@@ -25,9 +24,6 @@ sudo bash /tmp/nodesource_setup.sh && sudo apt-get -q install nodejs -y
 node -v
 npm -v
 
-sudo npm install neovim -g
-sudo apt-get -q -y install neovim
-
 out "Installing lazygit"
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
@@ -35,3 +31,10 @@ tar xf lazygit.tar.gz lazygit
 sudo install lazygit /usr/local/bin
 rm lazygit.tar.gz
 rm lazygit
+
+
+
+out "NVVVVIIIIMMM"
+curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
+./nvim.appimage --appimage-extract
+sudo mv squashfs-root/usr/bin/nvim /usr/bin/ 
