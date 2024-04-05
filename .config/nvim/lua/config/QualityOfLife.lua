f7a2d473b1535fed40f3dac56ec690debfbf23c9
ayyubi3 <eyup.ozturk@outlook.de>
2024-04-05 04:48:38 +0200

    sdfa

--- .config/nvim/lua/config/QualityOfLife.lua
+++ .config/nvim/lua/config/QualityOfLife.lua
@@ -1,5 +1,4 @@
 return {
-	"tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
 
 	{
 		"numToStr/Comment.nvim",
@@ -20,8 +19,67 @@ return {
 		end,
 	},
 	{
-		"m4xshen/hardtime.nvim",
-		dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
-		opts = {},
+		"eandrju/cellular-automaton.nvim",
+		dependencies = {
+			"tamton-aquib/duck.nvim",
+			"AndrewRadev/discotheque.vim",
+			"seandewar/killersheep.nvim",
+			{
+				"NStefan002/2048.nvim",
+				cmd = "Play2048",
+			},
+			"tamton-aquib/zone.nvim",
+		},
+		config = function()
+			local funcs = {
+
+				game_of_life = function()
+					vim.cmd("CellularAutomaton game_of_life")
+				end,
+
+				make_it_rain = function()
+					vim.cmd("CellularAutomaton make_it_rain")
+				end,
+				duck = function()
+					require("duck").hatch()
+				end,
+				duck_off = function()
+					require("duck").cook_all()
+				end,
+				disco = function()
+					vim.cmd("Disco e")
+				end,
+				disco_off = function()
+					vim.cmd("Disco")
+				end,
+				killersheep = function()
+					require("killersheep").setup()
+					vim.cmd("KillKillKill")
+				end,
+				_2048 = function()
+					require("2048").setup()
+					vim.cmd("Play2048")
+				end,
+				treadmill = function()
+					-- require("zone").setup({ after = 1, style = "treadmill" })
+				end,
+			}
+
+			local function random()
+				local keyset = {}
+				for k in pairs(funcs) do
+					table.insert(keyset, k)
+				end
+				local random_func = keyset[math.random(#keyset)]
+
+				funcs[random_func]()
+
+				vim.notify(vim.inspect(random_func))
+			end
+
+			vim.keymap.set("n", "<leader>rnd", function()
+				random()
+			end)
+		end,
 	},
 }
